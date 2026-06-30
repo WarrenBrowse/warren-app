b519970c8b11105ae56dcd6f2541c5cf04763e5d
poka <poka@p2p.legal>
2026-06-30 02:29:16 +0200

    warren CLI branding + headless daemon-only release workflow

--- mullvad-cli/src/main.rs
+++ mullvad-cli/src/main.rs
@@ -8,6 +8,7 @@ use cmds::*;
 pub const BIN_NAME: &str = env!("CARGO_BIN_NAME");
 
 #[derive(Debug, Parser)]
+#[command(name = "warren", bin_name = "warren")]
 #[command(version = mullvad_version::VERSION, about, long_about = None)]
 #[command(propagate_version = true)]
 enum Cli {
